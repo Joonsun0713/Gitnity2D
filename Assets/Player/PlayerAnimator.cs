b66c689512af03d13378e7f7a43b3d19069668a0
Joonsun0713 <vkfl7549@gmail.com>
2026-06-10 11:54:36 +0900

    Revert "Test_1"
    This reverts commit 0e883f6a762e5f6a0921c45b9aa7793e3a88f608, reversing
    changes made to 64283769f427ed24ccae0ce5b67aa4778b034ad3.

--- Assets/Player/PlayerAnimator.cs
+++ Assets/Player/PlayerAnimator.cs
@@ -6,66 +6,67 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     Animator animator;
-    PlayerAttack pattack;
+
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        pattack = GetComponentInChildren<PlayerAttack>();
     }
 
-    void PlayerAttackEnvent() // 콤보 공격 애니메이션 이벤트
+    // Update is called once per frame
+    void Update()
     {
-        pattack.Attack();
-        Debug.Log("공격 애니메이션 이벤트 성공");
-    }   
+        
+    }
 
-    public void SetMoveAnimation(bool isMoving) // 이동 애니메이션
+    public void SetMoveAnimation(bool isMoving)
     {
         
        animator.SetBool("Move", isMoving);
         
-    } 
+    }
 
     
-    public void PlayComboAttackAnimation(int ComboStep) // 콤보별 공격 애니메이션
+    public void PlayComboAttackAnimation(int ComboStep)
     {
         
         animator.SetInteger("ComboStep", ComboStep);
         animator.SetTrigger("Attack");
         
-    }   
+    }
 
-    public void PlayerJumpUpAnimation() // 점프 애니메이션
+    public void PlayerJumpUpAnimation()
     {
         animator.SetTrigger("Jump");
-    }   
+    }
 
-    public void PlayerJumpFallAnimation(bool IsFall) //낙하 애니메이션
+    public void PlayerJumpFallAnimation(bool IsFall)
     {
        
         animator.SetBool("Fall", IsFall);
         
-    }   
+    }
 
-    public void PlayerRollAnimation() //쉴드 애니메이션
+    public void PlayerRollAnimation()
     {
         animator.SetTrigger("Roll");
-    }   
+    }
 
-    public void PlayerShieldAnimation(bool isShield) //쉴드 애니메이션
+    public void PlayerShieldAnimation(bool isShield)
     {
         animator.SetBool("Shield",  isShield);
-    }   
+    }
 
-    public void PlayerDeathAnimation() // 사망 애니메이션
+    public void PlayerDeathAnimation()
     {
         animator.SetTrigger("Death");
-    }   
+    }
 
-    public void PlayerIsShieldAnimation() //튕겨내기 애니메이션
+    public void PlayerIsShieldAnimation()
     {
         animator.SetTrigger("IsShield");
-    }       
+    }
+
+   
 
 }
--- Assets/kΩsmaragd/SceneChange.cs
+++ Assets/kΩsmaragd/SceneChange.cs
@@ -1,31 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public string SC;       // SceneChange - �� �̵��� string
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    public string SC;       // SceneChange - 씬 이동용 string
     public void Load()
     {
-        SceneManager.LoadScene(SC);     // ���⼭ �̵���ų ���� �̸��� ������ �ȴ�.
+        string sceneName = EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log(sceneName);
+
+        if (sceneName == "Stage_1" || sceneName == "Stage_2" || sceneName == "Stage_3" || sceneName == "Stage_4" || sceneName == "Try_Button")
+            SC = "testSenes";   // 특정 스테이지로 가는 버튼일 경우 testScene 으로 가도록 설정
+                                // 추가 수정으로 새로운 스테이지를 만들 때마다, else if 를 추가하여 각각 새로운 스테이지를 이동하게끔 수정
+        
+        
+        else if (sceneName == "Start_Button" || sceneName == "Back_Button")
+            SC = "StageSelect"; // 스테이지 밖으로 벗아날 때, 또는 타이틀 화면에서 시작 버튼을 눌렀을 때, 스테이지 선택창으로 이동
+          
+        SceneManager.LoadScene(SC);     // 여기서 이동시킬 씬의 이름을 적으면 된다.
     }
 
-    public void GameEnd()
+    public void GameEnd()   // End 버튼 전용으로 게임을 종료할 때 사용.
     {
-        return;
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false; // 에디터에서 테스트 중일 때
+        #else
+            Application.Quit(); // 실제 빌드된 게임에서 종료할 때
+        #endif
     }
 }
